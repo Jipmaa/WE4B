8e2dac2588e56630b6c352dc4b63fcd9ea187dcb
Gaelle Le Boulicaut <lbg.gaelle@gmail.com>
2025-05-23 11:22:57 +0200

    barre de navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CoursComponent } from './cours/cours.component';
+import { ListecoursComponent } from './listecours/listecours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CoursComponent,
+    ListecoursComponent
   ],
   imports: [
     BrowserModule,
