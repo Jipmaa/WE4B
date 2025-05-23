efa913167b262b6f83c838689099df475680f7e2
Gaelle Le Boulicaut <lbg.gaelle@gmail.com>
2025-05-23 11:25:45 +0200

    barre de navigation

---
+++ src/app/listecours/listecours.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listecours',
+  templateUrl: './listecours.component.html',
+  styleUrls: ['./listecours.component.css']
+})
+export class ListecoursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
