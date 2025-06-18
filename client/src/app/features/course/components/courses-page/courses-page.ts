a48608d1e8cf673e36d53004abc91be3f4a9170b
Baptiste Pierson <baptistepierson2.0@gmail.com>
2025-06-18 04:58:53 +0200

    j'ai réussi à faire une box de cours youpi

--- client/src/app/features/course/components/courses-page/courses-page.ts
+++ client/src/app/features/course/components/courses-page/courses-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {CourseBox} from '@/features/course/components/courses-page/course-box/course-box';
 
 @Component({
   selector: 'app-courses-page',
-  imports: [],
+  imports: [CourseBox],
   templateUrl: './courses-page.html',
 })
 export class CoursesPage {
