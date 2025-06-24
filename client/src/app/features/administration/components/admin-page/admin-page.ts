f20d43044ebc0ef6adc704e53592f87a92291105
Gaelle Le Boulicaut <lbg.gaelle@gmail.com>
2025-06-24 17:37:16 +0200

    amélioration de la page admin

--- client/src/app/features/administration/components/admin-page/admin-page.ts
+++ client/src/app/features/administration/components/admin-page/admin-page.ts
@@ -1,40 +1,43 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { CommonModule } from '@angular/common';
 import { User } from '@/core/models/user.models';
-import { AdminPageList } from './admin-page-list/admin-page-list';
 import { UsersService } from '@/core/services/users.service';
 import { TabsComponent } from '@/shared/components/ui/tabs';
 import { TabItemComponent } from "../../../../shared/components/ui/tab-item/tab-item";
 import { TabContentComponent } from "../../../../shared/components/ui/tab-content/tab-content";
+import { CourseUnit } from '@/core/models/course-unit.models';
+import { CourseUnitsService } from '@/core/services/course-units.service';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule, AdminPageList, TabsComponent, TabItemComponent, TabContentComponent],
+  imports: [CommonModule, LucideAngularModule, TabsComponent, TabItemComponent, TabContentComponent],
   templateUrl: './admin-page.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AdminPage {
+export class AdminPage implements OnInit {
 
   UsersArray !: User[]
+  CoursesArray !: CourseUnit[]
 
-  showContent: string | null = null;
+  constructor(public servUsers: UsersService, private servCourse: CourseUnitsService) {
+    this.servUsers.getUsers().subscribe(
+      response => {
+        console.log(response);
+        this.UsersArray = response.data.users;
+      }
+    );
 
-  constructor(public servUe: UsersService) {
-      this.servUe.getUsers().subscribe(
-        response => {
-          console.log(response);
-          this.UsersArray = response.data.users;
-        }
-      )
-    }
-  
-    ngOnInit(): void {
-  
-    }
+    this.servCourse.getCourseUnits().subscribe(
+      response => {
+        console.log(response);
+        this.CoursesArray = response.data.courseUnits;
+      }
+    )
+  }
 
-  displayList(type: string): void {
-    this.showContent = type;
+  ngOnInit(): void {
+
   }
 }
