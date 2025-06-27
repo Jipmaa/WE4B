e6cfd94fbdf697853abc552b7cc24d7f54737acb
Nicolas Malewicz <nicolas.malewicz@gmail.com>
2025-06-27 16:54:49 +0200

    fix: rectification des boutons du forum

--- client/src/app/features/forum/components/discussion-page/discussion-page.component.ts
+++ client/src/app/features/forum/components/discussion-page/discussion-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DiscussionListComponent } from '../discussion-list/discussion-list.component';
 import { DiscussionViewComponent } from '../discussion-view/discussion-view.component';
 import { DiscussionCreationComponent } from '../discussion-creation/discussion-creation.component';
+import { IconButtonComponent } from '@/shared/components/ui/icon-button/icon-button';
 
 @Component({
   selector: 'app-discussion-page',
   standalone: true,
-  imports: [CommonModule, DiscussionListComponent, DiscussionViewComponent, DiscussionCreationComponent],
+  imports: [CommonModule, DiscussionListComponent, DiscussionViewComponent, DiscussionCreationComponent, IconButtonComponent],
   templateUrl: './discussion-page.component.html',
 })
 export class DiscussionPageComponent {
