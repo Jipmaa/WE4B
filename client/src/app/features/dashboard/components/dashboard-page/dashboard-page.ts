0e1a764f3fedf3ea863bef5ccda009e4a506ed77
Rémi Bernard <remib18@outlook.fr>
2025-06-28 18:02:17 +0200

    example
    Signed-off-by: Rémi Bernard <remib18@outlook.fr>

--- client/src/app/features/dashboard/components/dashboard-page/dashboard-page.ts
+++ client/src/app/features/dashboard/components/dashboard-page/dashboard-page.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarLayout } from '@/shared/components/layout/sidebar-layout/sidebar-layout';
+import {CourseBox} from '@/shared/components/ui/course-box/course-box';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
     CommonModule,
     SidebarLayout,
+    CourseBox,
   ],
   templateUrl: './dashboard-page.html',
 })
